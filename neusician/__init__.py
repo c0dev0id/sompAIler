d2cafc18bf3cd4678c17db6e41a4b62859a1af5f
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2021-04-04 18:13:56 +0200

    randomelody: sompyler_init= parameter, simple form
    Only when sompyler_init parameter is filled appropriately, you'll get a
    Sompyler score instead of plain JSON.
    
    E.g. to be filled with "2.0.1.0~2~6~120~100~96".
      2 Double the stress of beat 3
      0 unstressed
      1 simple stress
      0 unstressed
      2 divide every beat into two eighths
        (same can be achieved by "1.0")
        (divide it into 3 by three triplets: "3" or "1.0.0" or "2.1.0")
      6 cut first 6 ticks, so first note, assumed to have an offset of 0,
        actually gets an offset of 6.
      120 beats per minute
      100 %dB = 0dBFS for beat 1 with the maximal stress
      95 %dB = -5dBFS for unstressed beats 2, 4; beat 3 in between

--- neusician/__init__.py
+++ neusician/__init__.py
@@ -1,7 +1,12 @@
 import os
 from .arbitextonotes import tones
+from .sompyler_yaml import make_yaml_code
 from .markov_util import MarkovSpecError
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+users = {} # Just as long as we do not have a database
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,6 +15,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'neusician.db'),
     )
+
+    auth = HTTPBasicAuth(realm="Even more private an area")
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -36,12 +43,37 @@ def create_app(test_config=None):
             if len(seedphrase) > 1000:
                 return "Seed phrase is too long", 400
             try:
-                return jsonify(tones(
+                plaintones = tones(
                     request.form["seedphrase"],
                     request.form["markovspec"],
                     (int(request.form["melody-share"]),
                      int(request.form["pause-share"]))
-                    ))
+                    )
+                if "sompyler_init" in request.form:
+                    sompyler_init = request.form["sompyler_init"].split("~")
+                    subdivisions = sompyler_init[1]
+                    if subdivisions.isdecimal():
+                        subdivisions = [0] * int(subdivisions)
+                        subdivisions[0] += 1
+                    elif "." in subdivisions:
+                        subdivisions = [ int(x) for x in subdivisions.split(".") ]
+                    response = make_response(
+                        make_yaml_code(
+                            plaintones,
+                            beats=[
+                                int(x) for x in sompyler_init[0].split(".")
+                            ],
+                            subdivisions=subdivisions,
+                            cut=int(sompyler_init[2]),
+                            beats_per_minute=int(sompyler_init[3]),
+                            upper_stress_bound=int(sompyler_init[4]),
+                            lower_stress_bound=int(sompyler_init[5])
+                       ).getvalue(), 200)
+                    response.mimetype="text/plain"
+                    return response
+                else:
+                    return jsonify(plaintones)
+
             except MarkovSpecError as e:
                 return "Markov specification invalid: " + str(e), 400
         else:
@@ -55,10 +87,25 @@ def create_app(test_config=None):
                 melody_pause_ratio=(
                     request.args.get("melody-share", 1),
                     request.args.get("pause-share", 1)
-                )
+                ),
+                sompyler_init=request.args.get("sompyler_init")
             )
 
+    @auth.error_handler
+    def UnAuthorized(status):
+        if status == 401:
+            return render_template("401.tmpl")
+
+    @auth.verify_password
+    def verify_password(username, password):
+        if username.startswith("+"):
+            users[username[1:]] = generate_password_hash(password)
+        elif username in users and \
+                check_password_hash(users.get(username), password):
+                    return username
+
     @app.route('/sompyle', methods=('GET','POST'))
+    @auth.login_required
     def yaml_textarea():
         if request.method == 'POST':
             if request.form["submit"] == "payload":
@@ -67,7 +114,6 @@ def create_app(test_config=None):
         else: return render_template("yaml_input.tmpl",
                 yamlcode=request.args.get("yamlcode")
             )
-
 
     return app
 
---
+++ neusician/sompyler_yaml.py
@@ -0,0 +1,95 @@
+from io import StringIO
+
+def make_yaml_code(
+        tones, beats, subdivisions, cut, beats_per_minute,
+        upper_stress_bound, lower_stress_bound
+    ):
+
+    ticks_per_measure = len(beats) * len(subdivisions)
+    ticks_per_minute = beats_per_minute * len(subdivisions)
+    cut %= ticks_per_measure
+
+    measure_tones = {}
+
+    yaml = StringIO()
+    yaml.write(
+f"""
+# This is Sompyler/YAML script, a human- as well as machine-readable
+# music notation resoluble down to the level of exact sounding.
+# It can be converted to "draft music" (i.e. an audience would expect
+# better quality, but sound engineers are probably used to this kind of raw
+# material) by your local installation of Sompyler.
+#      Sompyler is available at <https://gitlab.com/flowdy/sompyler>.
+#      (Limited support to Python programmers via Gitlab issues only)
+#
+# Coming soon: https://demo.neusik.de/sompyle, an online test instance for
+#   users with a password. It will not be quite public, as the service is
+#   poor of resources and therefore asks for patience and resilience against
+#   being denied when no worker processess are available.
+#
+# Or, even better, make a sheet of it to play on your own instrument.
+#
+
+# Every measure contains all voices with notes in that measure. Notes
+# are indicated by following scheme:
+#     N: P L (N = offset from last bar, in ticks; P pitch; length, in ticks)
+#
+# A "tick" is the subdivision of the number of beats indicated in the
+# stress pattern. "cut" adds to the offset of the first note. There will not
+# be more pause before the note, only the stress of the note is adjusted
+# accordingly.
+
+stage:
+  p: 1|1 0 dev/piano # Well, this "piano" is pretty unsatisfactory in sound.
+                     # One may omit it alltogether, so Sompyler expects
+                     # a ...
+
+# ... free-style sound defined:
+instrument p: {} # So simple, it is only a single sine.
+
+# But, alas, this is not on sound, just on inspiration for composition.
+
+---
+_meta:
+  stress_pattern: {",".join(str(x) for x in beats)};{",".join(str(x) for x in subdivisions)}
+  ticks_per_minute: {ticks_per_minute}
+  upper_stress_bound: {upper_stress_bound}
+  lower_stress_bound: {lower_stress_bound}
+  cut: {cut}
+
+"""
+    )
+
+    following_measure = False
+
+    def skipper(skipped_measures):
+        nonlocal following_measure
+
+        if following_measure:
+            print("---", file=yaml)
+        else:
+            following_measure = True
+
+        if measure_tones: print("p: {", ", ".join(
+            ": ".join(str(x) for x in i) for i in measure_tones.items()
+        ), "}\n", file=yaml)
+
+        skipped_measures -= 1
+        measure_tones.clear()
+
+        for _ in range(skipped_measures):
+            print("---\n{} # let last tone sound\n", file=yaml)
+
+    for offset, pitch, length in tones:
+
+        offset += cut
+        skipped_measures, offset = divmod(offset, ticks_per_measure)
+
+        if skipped_measures: skipper(skipped_measures)
+
+        measure_tones[offset] = "{} {}".format(pitch, length)
+        cut = offset + length
+
+    skipper(int(cut % ticks_per_measure > 0) + cut // ticks_per_measure)
+
+    return yaml
