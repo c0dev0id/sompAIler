2624b00001abebba042fbc74f0e2f68c47aff044
c0dev0id <sh+git@codevoid.de>
2026-07-01 07:28:35 +0200

    dev-testserver: fix procman.SUBDIR to absolute path for schema.sql lookup

--- dev-testserver/wsgi.py
+++ dev-testserver/wsgi.py
@@ -1,8 +1,12 @@
 import importlib.util, pathlib
+import neusician.sompyler_procman as procman
 from neusician.server import create_app
 from jinja2 import FileSystemLoader, ChoiceLoader
 
 HERE = pathlib.Path(__file__).parent
+
+# SUBDIR is a CWD-relative constant in procman; fix it to an absolute path
+procman.SUBDIR = str(pathlib.Path(procman.__file__).parent)
 
 spec = importlib.util.spec_from_file_location('_config', HERE / 'instance' / 'config.py')
 mod = importlib.util.module_from_spec(spec)
